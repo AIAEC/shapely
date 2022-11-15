c6eb1fedef88b97ea42bce5ffcc74d673684740c
iwi <814140396@qq.com>
2022-11-15 15:20:17 +0800

    fix: fix prolong negative length

--- shapely/extension/util/prolong.py
+++ shapely/extension/util/prolong.py
@@ -3,6 +3,7 @@ from typing import Union, List
 from shapely.extension.model.vector import Vector
 from shapely.extension.typing import CoordType
 from shapely.geometry import Point, LineString
+from shapely.ops import substring
 
 
 def prolong(line: LineString,
@@ -25,7 +26,7 @@ def prolong(line: LineString,
 
     coords: List[CoordType] = list(line.coords)
 
-    if len(coords) < 2:
+    if len(coords) < 2:  # invalid or empty LineString
         return line
 
     def find_diff_coord_from(given_coord: CoordType, given_coords: List[CoordType], start_index: int, step: int):
@@ -43,13 +44,26 @@ def prolong(line: LineString,
     if not (coord_1 and coord_n2):
         return line
 
-    front_vec: Vector = Vector.from_origin_to_target(coord_1, coords[0]).unit(front_prolong_len)
-    end_vec: Vector = Vector.from_origin_to_target(coord_n2, coords[-1]).unit(end_prolong_len)
+    if front_prolong_len > 0:
+        front_vec: Vector = Vector.from_origin_to_target(coord_1, coords[0]).unit(front_prolong_len)
+        new_front: CoordType = front_vec.apply_coord(coords[0])
+        start_dist = 0  # start from new_front point
+    else:
+        new_front = coords[0]
+        start_dist = abs(front_prolong_len)  # if front_prolong_len is "-n", start dist is "n"
 
-    new_front: CoordType = front_vec.apply_coord(coords[0])
-    new_end: CoordType = end_vec.apply_coord(coords[-1])
+    if end_prolong_len > 0:
+        end_vec: Vector = Vector.from_origin_to_target(coord_n2, coords[-1]).unit(end_prolong_len)
+        new_end: CoordType = end_vec.apply_coord(coords[-1])
+        dist_to_end = 0  # end to new_end point
+    else:
+        new_end = coords[-1]
+        dist_to_end = abs(end_prolong_len)  # if end_prolong_len is "-n", end dist is prolonged.length - n
 
-    return LineString([new_front, *coords[1: -1], new_end])
+    prolonged = LineString([new_front, *coords[1: -1], new_end])
+
+    # for negative prolong_len, pick the substring from the prolonged
+    return substring(prolonged, start_dist=start_dist, end_dist=prolonged.length - dist_to_end, normalized=False)
 
 
 class Prolong:
--- tests/extension/test_prolong.py
+++ tests/extension/test_prolong.py
@@ -39,3 +39,23 @@ class ProlongTest(TestCase):
             'LINESTRING (-99.41728331859264 -81.86677583771929, 70.6436647152336 -84.15292082959837)')
         result = prolong(float_line, front_prolong_len=1e-3, end_prolong_len=1e-3)
         self.assertEqual(2, len(result.coords))
+
+    def test_prolong_negative_length(self):
+        line = LineString([(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)])
+        result = prolong(line, front_prolong_len=-1.5)
+        self.assertTrue(LineString([(1.5, 0), (2, 0), (2, 1), (2, 2)]).equals(result))
+
+        result = prolong(line, front_prolong_len=-2.5)
+        self.assertTrue(LineString([(2, 0.5), (2, 1), (2, 2)]).equals(result))
+
+        result = prolong(line, end_prolong_len=-2.5)
+        self.assertTrue(LineString([(0, 0), (1, 0), (1.5, 0)]).equals(result))
+
+        result = prolong(line, front_prolong_len=2, end_prolong_len=-5)
+        self.assertTrue(LineString([(-2, 0), (-1, 0)]).equals(result))
+
+        result = prolong(line, front_prolong_len=-3, end_prolong_len=-2)
+        self.assertTrue(LineString([(2, 1), (2, 0)]).equals(result))
+
+        result = prolong(line, front_prolong_len=-4, end_prolong_len=-4)
+        self.assertTrue(line.ext.inverse().equals(result))
