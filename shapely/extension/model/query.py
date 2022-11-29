5d8cbcdb933a00abf12268652e311a659f800592
iwi <814140396@qq.com>
2022-11-29 17:13:11 +0800

    feat: add add and remove to query

--- shapely/extension/model/query.py
+++ shapely/extension/model/query.py
@@ -12,21 +12,67 @@ class GeomQueryContainer(ABC):
     def query(self, geom: BaseGeometry) -> List[BaseGeometry]:
         raise NotImplementedError
 
+    @abstractmethod
+    def add(self, geom: BaseGeometry):
+        raise NotImplementedError
+
+    @abstractmethod
+    def remove(self, geom: BaseGeometry):
+        raise NotImplementedError
+
 
 class SeqQueryContainer(GeomQueryContainer):
     def __init__(self, geoms: List[BaseGeometry]):
         self._geoms = geoms
 
+    def add(self, geom: BaseGeometry):
+        if geom not in self._geoms:
+            self._geoms.append(geom)
+
+    def remove(self, geom: BaseGeometry):
+        if geom in self._geoms:
+            self._geoms.remove(geom)
+
     def query(self, geom: BaseGeometry) -> List[BaseGeometry]:
         return lfilter(lambda candidate: candidate.intersects(geom), self._geoms)
 
 
+class RTreeQueryContainer(GeomQueryContainer):
+    def __init__(self, geoms: List[BaseGeometry]):
+        self._db = STRtree(geoms)
+        self._added: List[BaseGeometry] = []
+        self._deleted: List[BaseGeometry] = []
+
+    def query(self, geom: BaseGeometry) -> List[BaseGeometry]:
+        result: List[BaseGeometry] = (
+                list(self._db.query(geom))
+                + lfilter(lambda candidate: candidate.intersects(geom), self._added))
+        return lfilter(lambda candidate: candidate not in self._deleted, result)
+
+    def add(self, geom: BaseGeometry):
+        existed = self.query(geom)
+        if geom not in existed and geom not in self._added:
+            self._added.append(geom)
+        if geom in self._deleted:
+            self._deleted.remove(geom)
+
+    def remove(self, geom: BaseGeometry):
+        if geom not in self._deleted:
+            self._deleted.append(geom)
+
+
 class Query:
-    def __init__(self, geoms: Sequence[BaseGeometry], container_type: type = STRtree):
+    def __init__(self, geoms: Sequence[BaseGeometry], container_type: type = RTreeQueryContainer):
         if any(not isinstance(geom, BaseGeometry) for geom in geoms):
             raise TypeError('expect given geoms are all geometry objects')
         self._geoms = list(geoms)
         self._db = container_type(self._geoms)
+
+    def add(self, geom):
+        self._db.add(geom)
+
+    def remove(self, geom):
+        self._db.remove(geom)
 
     @classmethod
     def from_flatten(cls, geoms: Sequence[BaseGeometry], container_type: type = STRtree) -> 'Query':
