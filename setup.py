1e4d40819925de1040150803d65c1b04089024ee
iwi <814140396@qq.com>
2022-12-09 09:56:51 +0800

    fix: add python3.11 support

--- setup.py
+++ setup.py
@@ -203,6 +203,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering :: GIS',
     ],
     cmdclass           = {},
