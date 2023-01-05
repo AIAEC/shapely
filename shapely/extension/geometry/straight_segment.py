2e87c961cefbbe5db48cd7d5bed92d63ff4efb3f
iwi <814140396@qq.com>
2023-01-05 18:12:42 +0800

    refactor: enhance straight segment

--- shapely/extension/geometry/straight_segment.py
+++ shapely/extension/geometry/straight_segment.py
@@ -12,3 +12,45 @@ class StraightSegment(LineString):
         coords_list = list(self.coords)
         if len(coords_list) != 2 and coordinates is not None:
             raise ValueError(f'input coordinates must have 2 coordinates, given {coords_list}')
+
+    def point_on_left(self, point: 'Point') -> bool:
+        """
+        check if given point is on the left side of this segment
+        Parameters
+        ----------
+        point: Point
+
+        Returns
+        -------
+        bool
+        """
+        coords = list(self.coords)
+        return (coords[1][0] - coords[0][0]) * (point.y - coords[0][1]) - (coords[1][1] - coords[0][1]) * (point.x - coords[0][0]) > 0
+
+    def point_on_right(self, point: 'Point') -> bool:
+        """
+        check if given point is on the right side of this segment
+        Parameters
+        ----------
+        point: Point
+
+        Returns
+        -------
+        bool
+        """
+        coords = list(self.coords)
+        return (coords[1][0] - coords[0][0]) * (point.y - coords[0][1]) - (coords[1][1] - coords[0][1]) * (point.x - coords[0][0]) < 0
+
+    def point_on_line(self, point: 'Point') -> bool:
+        """
+        check if given point is on the line of this segment
+        Parameters
+        ----------
+        point: Point
+
+        Returns
+        -------
+        bool
+        """
+        coords = list(self.coords)
+        return (coords[1][0] - coords[0][0]) * (point.y - coords[0][1]) - (coords[1][1] - coords[0][1]) * (point.x - coords[0][0]) == 0
--- tests/extension/test_straight_segment.py
+++ tests/extension/test_straight_segment.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 from unittest import TestCase
 
 from shapely.extension.geometry.straight_segment import StraightSegment
+from shapely.geometry import Point
 
 
 class StraightSegmentTest(TestCase):
@@ -21,3 +22,17 @@ class StraightSegmentTest(TestCase):
         line3 = deepcopy(line2)
         self.assertEqual(line2, line3)
 
+    def test_point_on_left(self):
+        line = StraightSegment([(0, 0), (1, 0)])
+        self.assertTrue(line.point_on_left(Point(0, 0.5)))
+        self.assertFalse(line.point_on_left(Point(0, -0.5)))
+
+    def test_point_on_right(self):
+        line = StraightSegment([(0, 0), (1, 0)])
+        self.assertTrue(line.point_on_right(Point(0, -0.5)))
+        self.assertFalse(line.point_on_right(Point(0, 0.5)))
+
+    def test_point_on_line(self):
+        line = StraightSegment([(0, 0), (1, 0)])
+        self.assertTrue(line.point_on_line(Point(0.5, 0)))
+        self.assertFalse(line.point_on_line(Point(0, 0.5)))
