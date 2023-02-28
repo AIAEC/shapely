cf985ba83afc43c635bf2697a7910dfae5aa52bd
iwi <814140396@qq.com>
2023-02-28 17:57:16 +0800

    feat: observer ;

---
+++ shapely/extension/model/observer.py
@@ -0,0 +1,3 @@
+# one observer can hold a geometry object.
+# one observer can affect one of more observers.
+# several observers can form a multi-observer, which can affect one of more observers also.
