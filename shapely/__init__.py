1c43b6ce9bab0ab714c891464358902006da22e5
iwi <814140396@qq.com>
2023-03-08 00:04:12 +0800

    refactor: suppress shapely warning

--- shapely/__init__.py
+++ shapely/__init__.py
@@ -1,2 +1,9 @@
-__version__ = "1.8.5.25.dev4"
+__version__ = "1.8.5.25.dev5"
+
 # NOTICE: only change the last number for our customized shapely packages
+
+
+# suppress annoying ShapelyDeprecationWarning
+import warnings
+
+warnings.filterwarnings(action='ignore', category=FutureWarning)
