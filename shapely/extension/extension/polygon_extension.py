6397f17ac4bbfdc70859c97cfc069803a589162b
iwi <814140396@qq.com>
2023-04-19 12:49:05 +0800

    fix: fix union of poly extension

--- shapely/extension/extension/polygon_extension.py
+++ shapely/extension/extension/polygon_extension.py
@@ -1,7 +1,7 @@
 from itertools import product
 from typing import Union, Optional, Iterable, Tuple, List
 
-from shapely.extension.constant import MATH_EPS
+from shapely.extension.constant import MATH_EPS, MATH_MIDDLE_EPS
 from shapely.extension.extension.base_geom_extension import BaseGeomExtension
 from shapely.extension.geometry.straight_segment import StraightSegment
 from shapely.extension.model.vector import Vector
@@ -9,6 +9,7 @@ from shapely.extension.strategy.decompose_strategy import BaseDecomposeStrategy
 from shapely.extension.util.ccw import ccw
 from shapely.extension.util.decompose import decompose
 from shapely.extension.util.polygon_cutter import PolygonCutter
+from shapely.extension.util.union import tol_union
 from shapely.geometry import Polygon, LineString, JOIN_STYLE, CAP_STYLE, MultiPolygon, Point
 from shapely.ops import unary_union, nearest_points
 
@@ -139,7 +140,7 @@ class PolygonExtension(BaseGeomExtension):
         -------
         polygon or multi-polygon
         """
-        union_polys = self._geom.union(poly).ext.flatten(Polygon).list()
+        union_polys = tol_union([self._geom, poly], MATH_MIDDLE_EPS).ext.flatten(Polygon).list()
         if len(union_polys) == 1:
             return union_polys[0]
 
