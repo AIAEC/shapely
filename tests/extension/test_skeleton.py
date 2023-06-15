3242002c83a95da3921c7dbf3258dc49ef7c2022
iwi <814140396@qq.com>
2023-06-15 14:06:59 +0800

    fix: remove skeleton test

--- tests/extension/test_skeleton.py
+++ tests/extension/test_skeleton.py
@@ -15,9 +15,6 @@ def poly_with_holes() -> Polygon:
     return loads(
         'POLYGON ((210 360, 30 290, 30 100, 170 30, 150 150, 350 150, 290 30, 400 30, 450 230, 378 365, 210 360), (200 310, 110 280, 64 154, 110 100, 130 240, 350 250, 390 180, 420 220, 365 323, 200 310), (210 200, 210 180, 340 180, 330 220, 210 200))')
 
-@pytest.fixture
-def weired_poly() -> Polygon:
-    return loads('POLYGON ((651.5863368575259 80.22196853466632, 650.7416117319614 79.37797631322371, 650.7394911998296 79.38009842108187, 650.7394904116246 79.38009763356077, 650.4571187632531 79.66267910682919, 649.5383342155666 78.74389455914276, 649.1848984422387 79.09716418949293, 650.1034821363093 80.01614926107754, 650.4571187630206 79.66267910706185, 649.7499086557777 80.37041472842321, 650.5942732131804 81.21404669457891, 651.5863368575259 80.22196853466632))')
 
 class TestSkeletonForSimpleCase:
     def test_trunk_segments_of_polygon(self, simple_poly):
@@ -65,15 +62,3 @@ class TestSkeletonForPolygonWithHoles:
         skeleton = Skeleton(poly)
         trunks = skeleton.trunks()
         assert len(trunks) == 5
-
-class TestSkeletonForWeiredPoly1:
-    def test_full_segments(self, weired_poly):
-        poly = weired_poly
-        print(poly.is_valid)
-        print(poly.is_empty)
-        dist = poly.minimum_clearance
-        try:
-            Skeleton(poly.buffer(dist).buffer(-dist))
-        except Exception:
-            pass
-        print('continue')
