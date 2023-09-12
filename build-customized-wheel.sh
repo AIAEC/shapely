47cfbd7a3008680be0a0c8babd9cfa421ab6457a
iwi <814140396@qq.com>
2023-09-12 20:13:21 +0800

    fix: fix building bug

--- build-customized-wheel.sh
+++ build-customized-wheel.sh
@@ -8,13 +8,12 @@ UNREPAIRED_WHEELS=/tmp/wheels
 SUPPORT_PY_VERSIONS=(cp38-cp38 cp39-cp39 cp310-cp310 cp311-cp311)
 for PY_VERSION in ${SUPPORT_PY_VERSIONS[@]}; do
     echo "working on ${PY_VERSION}"
-    /opt/python/${PY_VERSION}/bin/python setup.py bdist_wheel -d ${UNREPAIRED_WHEELS}
+    PYBIN=/opt/python/${PY_VERSION}/bin/python
+    PATH=${PYBIN}:$PATH
+    ${PYBIN} setup.py bdist_wheel -d ${UNREPAIRED_WHEELS}
 done
 
 # Bundle GEOS into the wheels
 for whl in ${UNREPAIRED_WHEELS}/*.whl; do
     auditwheel repair ${whl} -w dist
 done
-
-# build source distribution using python3.10
-/opt/python/cp310-cp310/bin/python3 setup.py sdist
